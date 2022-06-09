17615bdd10be890b2dca75cb055031227b6184f6
toilatoan <toilatoan@gmail.com>
2022-06-09 23:29:06 +0700

    add file ddl.sql first

---
+++ DDL.sql
@@ -0,0 +1,22 @@
+create database reminder;
+use reminder;
+
+create table `task` (
+	`task_no` int primary key auto_increment,
+    `title` varchar(200) not null,
+    `description` varchar(500),
+    `date_reminder` datetime not null,
+    `status` int
+);
+
+create table `setting` (
+	`noti_time` int not null,
+    `record_per_page` int default 10,
+    `date_format` varchar(20),
+    `mode` int default 0
+);
+
+create table `status` (
+	`id` int primary key,
+    `color_code` varchar(20) unique
+);
