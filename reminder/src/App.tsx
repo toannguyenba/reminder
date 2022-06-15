e8917731e6baeba1cdae97b1fb90a9c93f7a7304
Nguyen Ba Toan <nguyenbatoan@luvina.net>
2022-06-15 09:03:59 +0700

    remove sample code

--- reminder/src/App.tsx
+++ reminder/src/App.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Clock from './components/Clock';
-import LineItem from './components/LineItem';
 
 function App() {
   return (
     <div className="App">
-      <Clock/>
-      <LineItem name="Default name"/>
     </div>
   );
 }
